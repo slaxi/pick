ae132f8f8b2859fe0a13bf470404c0d8edbbe702
Nenad Ivkovic <nenad_ivkovic@epam.com>
2025-05-14 16:54:03 +0200

    Should expand props that video wrapper can take

--- src/components/video/video-wrapper/VideoWrapper.tsx
+++ src/components/video/video-wrapper/VideoWrapper.tsx
@@ -1,18 +1,54 @@
+// import React from "react";
+// import Video from "../video/Video";
+// import Title from "../../title/Title";
+// import VideoOverlay from "../video-overlay/VideoOverlay";
+// import Text from "../../text/Text";
+// import "../../video/Video.css";
+
+// type TVideoWrapper = React.ComponentProps<typeof Video> & {
+//   children?: React.ReactNode;
+// };
+
+// const VideoWrapper: React.FC<TVideoWrapper> = ({ children, ...props }) => {
+//   console.log("DJOKA:", props);
+//   return (
+//     <>
+//       {children}
+//       <Video {...props}>
+//         <VideoOverlay />
+//         <Title heading="h2" fontSize={64} className="video_heading font-sans">
+//           This is the heading
+//           <Text className="text-white-800 font-sans font-thin" fontSize={18}>
+//             <span>
+//               Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ullam,
+//               blanditiis odio!
+//             </span>
+//           </Text>
+//         </Title>
+//       </Video>
+//     </>
+//   );
+// };
+
+// export default VideoWrapper;
+
 import React from "react";
 import Video from "../video/Video";
 import Title from "../../title/Title";
 import VideoOverlay from "../video-overlay/VideoOverlay";
 import Text from "../../text/Text";
-import '../../video/Video.css'
+import "../../video/Video.css";
 
-const VideoWrapper = ({ children }: { children?: React.ReactNode }) => {
+// Nasleđivanje propova iz Video komponente
+type TVideoWrapper = React.ComponentProps<typeof Video> & {
+  children?: React.ReactNode;
+};
+
+const VideoWrapper: React.FC<TVideoWrapper> = ({ children, ...props }) => {
   return (
     <>
       {children}
-      <Video
-        src="https://cdn.pixabay.com/video/2025/04/10/271161_tiny.mp4"
-        className="video_fullscreen"
-      >
+      <Video {...props}>
         <VideoOverlay />
         <Title heading="h2" fontSize={64} className="video_heading font-sans">
           This is the heading
